dcec758abf6ea6edd926ebcbb47b2055f4ab79ea
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2018-12-30 18:55:47 -0700

    Feat do some more work

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,12 +4,13 @@ import React, {
 
 class Counter extends Component {
 
+  /*
   render() {
     return(
       <h1> Hello World</h1>
     )
   }
-  /*
+  */
   render() {
     return (
       showDecks({atleastOneDeck:true})
@@ -19,21 +20,10 @@ class Counter extends Component {
   showDecks(props) {
     const atleastOneDeck = props.atleastOneDeck;
     if (atleastOneDeck) {
-      return yesDecksYet();
+      return <h1>Some decks have been created!</h1>;
     }
-    return noDecksYet();
+    return <h1>No decks created yet!</h1>;;
   }
-
-  noDecksYet() {
-    return <h1>No decks created yet!</h1>;
-  }
-
-  yesDecksYet() {
-    return <h1>Some decks have been created!</h1>
-    // I'll work on displaying the decks later but currently i think i should work on creating definitions and then creating sets (terms + definitions) and then I can worry about counting the amount of sets
-  }
-  */
-
 }
 
 export default Counter
---
+++ src/newSet.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from './components/counter';
+import NewSet from './components/newSet';
+
+//ReactDOM.render( < NewSet / > , document.getElementById('createNewSet'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
